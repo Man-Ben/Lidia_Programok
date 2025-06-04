803aafd5638308118248cab82a0f601a1b41033e
Man-Ben <manbeniamin15@gmail.com>
2025-06-04 15:30:27 +0300

    Egyszerusitve

--- 2.cpp
+++ 2.cpp
@@ -4,17 +4,15 @@ int main(){
     int n, min=0, max=0;
     cin >> n;
     int a[n];
-    for(int i=0; i<=n-1; i++){
+    for(int i=0; i<=n-1; i++)
         cin >> a[i];
-        if(i==0){
-            min=a[i];
-            max=a[i];
-        } else {
+    min=a[0];
+    max=a[0];
+    for(int i=0; i<=n-1; i++){
             if(a[i]<min) 
                 min=a[i];
             if(a[i]>max) 
                 max=a[i];
-        }
     }
     cout << "Minimul din sirul a: " << min << " Maximul din sirul a: " << max << endl;
     return 0;
