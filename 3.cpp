680c26270ccbfc3cd6049676f3f42350b0468e63
Man-Ben <manbeniamin15@gmail.com>
2025-06-04 15:25:49 +0300

    Keeeeesz

--- 3.cpp
+++ 3.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n, max=0, nr=0, i, j;
+    int n, div=0, i, j, k=0;
     cin >> n;
     int a[n];
+    int b[n];
     for(i=0; i<=n-1; i++)
         cin >> a[i];
-for(j=0; j<=n-1; j++){
-    i=1;
-    if(i*i<=a[j]){
-        while(a[j]%i==0){
-            if(a[j]/i!=i)
-                nr+=2;
-            if(a[j]/i==i)
-                nr++;
+    for(i=0; i<=n-1; i++){
+        div=0;
+        for(j=1; j*j<=a[i]; j++){
+            if(a[i]%j==0){
+                if(a[i]/j==j)
+                    div++;
+                else
+                    div+=2;
+            }
+            if(div>2)
+            b[k++]=a[i];
         }
-        i++;
     }
-}
-    cout << max << endl;
+    int min=b[0];
+    for(i=0; i<=k-1; i++){
+        if(b[i]<min)
+            min=b[i];
+    }
+    cout << min << endl;    
     return 0;
 }
