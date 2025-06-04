ee89a12a056cf9eb0f70df17f670840ea3b622e5
Man-Ben <manbeniamin15@gmail.com>
2025-06-04 15:31:55 +0300

    Egyszerusitett

--- 4.cpp
+++ 4.cpp
@@ -4,16 +4,15 @@ int main(){
     int n, max=0, nr=0;
     cin >> n;
     int a[n];
-    for(int i=0; i<=n-1; i++){
+    for(int i=0; i<=n-1; i++)
         cin >> a[i];
-        if(i==0){
-            max=a[i];
-        }
+        max=a[0];
+    for(int i=0; i<=n-1; i++){
         if(a[i]>max)
             max=a[i];
         if(a[i]==max)
             nr++;
     }
-    cout << "Maximul din sirul a este: " << max << " Si apare de" << nr << " ori"<< endl;
+    cout << "Maximul din sirul a este: " << max << " Si apare de " << nr << " ori"<< endl;
     return 0;
 }
