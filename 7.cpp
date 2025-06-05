71fb8ebf34ec8977a15f3ee07a2fa123a452cb52
Man-Ben <manbeniamin15@gmail.com>
2025-06-05 14:30:41 +0300

    Kesz

---
+++ 7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, nr=0, i;
+    cin >> n;
+    int a[n];
+    for(i=0; i<=n-1; i++)
+        cin >> a[i];
+    for(i=0; i<=n-1; i++)
+        if(a[i]%2==0 && a[i+1]%2!=0)
+            nr++;
+    if(nr==n/2)
+        cout << "Da";
+    else 
+        cout << "Nu";
+    return 0;
+}
